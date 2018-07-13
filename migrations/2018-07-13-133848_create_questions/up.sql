3cc18a773c9875ca7d2d027867f22b2f5cecf2ab
Julius Naperkowski <julius@nepos.io>
2018-07-13 15:51:18 +0200

    created migration for quiz questions

---
+++ migrations/2018-07-13-133848_create_questions/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY,
+  questiontext TEXT NOT NULL,
+  optiona TEXT NOT NULL,
+  optionb TEXT NOT NULL,
+  optionc TEXT NOT NULL,
+  optiond TEXT NOT NULL
+)
---
+++ src/schema.rs
@@ -0,0 +1,10 @@
+table! {
+    questions (id) {
+        id -> Int4,
+        questiontext -> Text,
+        optiona -> Text,
+        optionb -> Text,
+        optionc -> Text,
+        optiond -> Text,
+    }
+}
