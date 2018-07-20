9bd882b6ea9fac900af26ee8a910d68c0c1f5d38
Julius Naperkowski <julius@nepos.io>
2018-07-20 12:51:40 +0200

    added routing for our service

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@ extern crate diesel;
 extern crate serde;
 extern crate serde_json;
 extern crate quizduell;
+extern crate futures;
 
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Request, Response, Server, Method, StatusCode};
 use hyper::rt::Future;
-use hyper::service::service_fn_ok;
+use hyper::service::service_fn;
 use diesel::prelude::*;
 use quizduell::establish_connection;
 use quizduell::models::Question;
+use futures::future;
 
-fn get_questions(_req: Request<Body>) -> Response<Body> {
+fn get_questions() -> String {
     use quizduell::schema::questions::dsl::*;
 
     let connection = establish_connection();
@@ -20,8 +22,31 @@ fn get_questions(_req: Request<Body>) -> Response<Body> {
         .load::<Question>(&connection)
         .expect("Error loading questions");
 
-    Response::new(Body::from(serde_json::to_string(&results).unwrap()))
+    serde_json::to_string(&results).unwrap()
 }
+
+
+// Just a simple type alias
+type BoxFut = Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>;
+
+fn quizduell(req: Request<Body>) -> BoxFut {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from("Try GETting data from /questions");
+        },
+        (&Method::GET, "/questions") => {
+            *response.body_mut() = Body::from(get_questions());
+        },
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        },
+    };
+
+    Box::new(future::ok(response))
+}
+
 
 fn main() {
     // This is our socket address...
@@ -31,7 +56,7 @@ fn main() {
     // creates on of our `hello_world` function.
     let get_questions_svc = || {
         // service_fn_ok converts our function into a `Service`
-        service_fn_ok(get_questions)
+        service_fn(quizduell)
     };
 
     let server = Server::bind(&addr)
