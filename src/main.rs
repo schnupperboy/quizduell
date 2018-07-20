0d7247359c851120894fbb0989cf7f1654d4f918
Julius Naperkowski <julius@nepos.io>
2018-07-20 11:36:50 +0200

    added hyper http server with first dummy endpoint for showing questions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate hyper;
+
+use hyper::{Body, Request, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+
+const PHRASE: &str = "is this a placeholder question?";
+
+fn get_questions(_req: Request<Body>) -> Response<Body> {
+    Response::new(Body::from(PHRASE))
+}
+
 fn main() {
-    println!("Hello, world!");
+    // This is our socket address...
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    // A `Service` is needed for every connection, so this
+    // creates on of our `hello_world` function.
+    let get_questions_svc = || {
+        // service_fn_ok converts our function into a `Service`
+        service_fn_ok(get_questions)
+    };
+
+    let server = Server::bind(&addr)
+        .serve(get_questions_svc)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    // Run this server for... forever!
+    hyper::rt::run(server);
 }
